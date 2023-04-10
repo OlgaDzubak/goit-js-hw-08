6a49525d10f61bee431b51c96fe32efaa8b9d317
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-04-10 16:05:40 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,12 @@ import throttle from "lodash/throttle";
 const iframe = document.querySelector('iframe');
 const player = new vimeoPlayer(iframe);
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const current_time = localStorage.getItem(LOCALSTORAGE_KEY);
 
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
-
+if !(current_time){ current_time = 0; }
+ 
+player.setCurrentTime(current_time);
 player.on('timeupdate', throttle(onTimeUpdate, 1000));
-
-function onTimeUpdate(){
+unction onTimeUpdate(){
     player.getCurrentTime().then(seconds => {localStorage.setItem(LOCALSTORAGE_KEY, seconds);});
 }
